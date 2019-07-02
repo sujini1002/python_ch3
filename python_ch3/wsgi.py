816c3c3a8d3d0015fddea2a070c04d69388e873a
sujini <tgif20146@gmail.com>
2019-07-02 15:48:43 +0900

    sys append path

--- python_ch3/wsgi.py
+++ python_ch3/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+import sys
 import os
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_ch3.settings')
